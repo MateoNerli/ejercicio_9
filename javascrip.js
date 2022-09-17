4032825d68203e217da2fb1749226f03b23d1908
mateo <mnerli2003@gmail.com>
2022-09-16 21:40:05 -0300

    agregar

---
+++ javascrip.js
@@ -0,0 +1,10 @@
+var pais = "argentina";
+var continente = "america";
+var año = 2022;
+ciudad = "san nicolas";
+provincia = "buenos aires";
+var completo = continente + " " + pais + " " + provincia + " " +ciudad;
+
+console.log(continente, pais, provincia, ciudad);
+
+alert(completo);
